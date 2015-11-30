80b518cd1f1623e9c082fab3d4a978c01fc0e01f
Koichi Higashi <khigashi@bio.titech.ac.jp>
2015-11-30 22:25:24 +0900

    Create imatrix.h

---
+++ imatrix.h
@@ -0,0 +1,10 @@
+/*
+    imatrix.h
+    a header file of int matrix
+*/
+#ifndef IMATRIX_H
+#define IMATRIX_H
+
+extern int **imatrix(int rows, int cols);
+extern void free_imatrix(int **matrix, int rows);
+#endif
