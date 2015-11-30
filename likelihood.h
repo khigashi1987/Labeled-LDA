fde642aaaaf78ffb15b923b268985ed1b0d0342e
Koichi Higashi <khigashi@bio.titech.ac.jp>
2015-11-30 22:26:41 +0900

    Create likelihood.h

---
+++ likelihood.h
@@ -0,0 +1,9 @@
+/*
+    likelihood.h
+*/
+#ifndef LIKELIHOOD_H
+#define LIKELIHOOD_H
+
+extern double loglikelihood(int **n_mz, int **n_zw, int nclass, int nlex, int ndocs, double alpha, double beta);
+
+#endif
