bb8d058010265d6db6492d91b8a9eb1cbe56eaa7
Koichi Higashi <khigashi@bio.titech.ac.jp>
2015-11-30 22:27:20 +0900

    Create llda.h

---
+++ llda.h
@@ -0,0 +1,16 @@
+/*
+    llda.h
+    header file for Labeled-LDA
+*/
+#ifndef LLDA_H
+#define LLDA_H
+#include <stdlib.h>
+#include "feature.h"
+
+#define MAXITER_DEFAULT 30
+#define ALPHA_DEFAULT 0.1
+#define BETA_DEFAULT 0.1
+
+void usage(void);
+
+#endif
