b4a2ac28359b86b8689fee160ad57292864a27a6
Koichi Higashi <khigashi@bio.titech.ac.jp>
2015-11-30 22:26:03 +0900

    Create learn.h

---
+++ learn.h
@@ -0,0 +1,12 @@
+/*
+    learn.h
+*/
+#ifndef LEARN_H
+#define LEARN_H
+#include <stdlib.h>
+#include "feature.h"
+
+#define RANDOM ((double)rand()/(double)RAND_MAX)
+
+extern void llda_learn(document *data, double alpha, double beta, int nclass, int nlex, int dlenmax, int maxiter, double **phi, double **theta, FILE *likp);
+#endif
