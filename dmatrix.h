095acf817d8dc2e6e627c7539c009c709ad6cbc4
Koichi Higashi <khigashi@bio.titech.ac.jp>
2015-11-30 22:23:55 +0900

    Create dmatrix.h

---
+++ dmatrix.h
@@ -0,0 +1,10 @@
+/*
+    dmatrix.h
+    a header file of double matrix
+*/
+#ifndef DMATRIX_H
+#define DMATRIX_H
+
+extern double **dmatrix(int rows, int cols);
+extern void free_dmatrix(double **matrix, int rows);
+#endif
